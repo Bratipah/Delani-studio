8d65476a57f063dd61b51b9a9de151e9c1b49bd5
Bratipah <bratipahmh@gmail.com>
2020-08-31 23:45:07 +0300

    add jquery code of dev and prod.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,33 @@
+$(document).ready(function () { //design fuction and toggling
+    $("#design-image").click(function () {
+        $("#design").toggle();
+    });
+    $("#design").click(function () {
+        $("#design-image").toggle();
+
+    });
+});
+
+
+//development image and info toggle
 $(document).ready(function () {
-    $(".image").click(function () {
-        $(".info").toggle();
-    })
-})
+    $("#development-image").click(function () {
+        $("#development").toggle();
+    });
+    $("#development").click(function () {
+        $("#development-image").toggle();
+
+    });
+});
+
+
+//production image and toggling info
+$(document).ready(function () {
+    $("#production-image").click(function () {
+        $("#production").toggle();
+    });
+    $("#production").click(function () {
+        $("#production-image").toggle();
+
+    });
+});
